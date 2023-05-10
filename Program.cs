8664091b9714f9b867ee613168d23b91e0842e4d
Flavio Ramon <flavioramonss@gmail.com>
2023-05-09 23:00:45 -0300

    Adição novo carro

--- Program.cs
+++ Program.cs
@@ -8,14 +8,22 @@ public class Principal {
         var result = Sum(7, 3);
         Console.WriteLine(result);
 
-        var car = new Car {
+        var palio = new Car {
             Code = 1234,
             Name = "Palio",
             Brand = "Fiat",
             Year = 2010
         };
+        var voyage = new Car {
+            Code = 4321,
+            Name = "voyage",
+            Brand = "volkswagen",
+            Year = 2010
+        };
 
-        Console.WriteLine(car);
+        Console.WriteLine(palio);
+        Console.WriteLine(voyage);
+        
     }
 
     private static int Sum(int a, int b) {
